7ea8512ef1848db502f28045aa88cb1ef7e718f4
Constantin <constantin.burger@hetic.net>
2018-01-12 16:14:45 +0100

    Adding Intro and Menu

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="intro">
-    <p>Intro</p>
+
+  <div class="vue intro">
+     <div class="intro__content">
+        <p class="intro__content-text">
+           The real hero is... Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           Vivamus quis neque sem. Quisque quis accumsan sapien. Nunc pellentesque elit
+           sed auctor rhoncus.
+        </p>
+        <div class="intro__content-scroll">
+           <p class="intro__content-scroll-text">
+             Scroll to<br> explore
+           </p>
+           <div class="intro__content-scroll-line"></div>
+        </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -14,10 +28,64 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-.landing {
-    p {
-        font-size: 60px;
-    }
+<style lang="scss">
+
+/* A SUPPRIMER */
+
+body{
+   height: 100vh;
+   margin: 0;
+   padding: 0;
 }
+
+#app, .vue{
+   height: 100%;
+   width: 100%;
+}
+
+/* FIN A SUPPRIMER */
+
+.intro{
+   width: 100%;
+   height: 100%;
+   background-color: #000000;
+}
+
+.intro__content{
+   position: relative;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   width: 300px;
+   height: 100%;
+   margin: 0 auto;
+}
+.intro__content-text{
+   width: 100%;
+   padding: 35px 25px 0;
+   font-size: 20px;
+   text-align: center;
+   color: #ffffff;
+   line-height: 1.5;
+   font-family: SourceSansPro, Arial;
+}
+
+.intro__content-scroll{
+   position: absolute;
+   bottom: 0;
+   left: 0;
+   width: 100%;
+   text-align: left;
+}
+.intro__content-scroll-text{
+   font-size: 14px;
+   color: #ffffff;
+   margin-bottom: 12px;
+}
+.intro__content-scroll-line{
+   width: 2px;
+   height: 75px;
+   background-color: #ffffff;
+}
+
 </style>
---
+++ src/components/menu.vue
@@ -0,0 +1,78 @@
+<template>
+
+   <div class="hmenu">
+      <div class="hmenu__main">
+         <h1 class="hmenu__main-title">
+            The Real header-footer-settings
+         </h1>
+         <div class="hmenu__main-button">
+            <span></span>
+            <span></span>
+            <span></span>
+         </div>
+      </div>
+      <div class="hmenu__toggle">
+         <ul class="hmenu__toggle-menu">
+            <li class="hmenu__toggle-menu-item">Stories</li>
+            <li class="hmenu__toggle-menu-item">About</li>
+            <li class="hmenu__toggle-menu-item">Share</li>
+         </ul>
+      </div>
+   </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Intro',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+.hmenu{
+
+}
+
+.hmenu__main{
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   width: 100%;
+   padding: 20px;
+}
+.hmenu__main-title{
+
+}
+.hmenu__main-button{
+
+}
+.hmenu__main-button span{
+   width: 2px;
+   height: 2px;
+   border-radius: 2px;
+   margin-bottom: 7px;
+}
+.hmenu__main-button:last-child{
+   margin-bottom: 0;
+}
+
+.hmenu__main-button.active span{width: 30px; border-radius: 2px;}
+.hmenu__main-button.active span + span{width: 38px;}
+.hmenu__main-button.active span + span + span{width: 23px;}
+
+.hmenu__toggle{
+
+}
+.hmenu__toggle-menu{
+
+}
+.hmenu__toggle-menu-item{
+
+}
+
+</style>
