129ddfc91cf2df4682627a204d3e2dba32f7798c
Axel <axel.tonson@hetic.net>
2018-01-12 11:12:23 +0100

    creation de la route landing, ajout du scss

---
+++ src/components/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="landing">
+    <p>coucou</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.landing {
+    p {
+        font-size: 60px;
+    }
+}
+</style>
