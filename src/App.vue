a126d0e4a03a4e9597b1a740c55b67765f3411eb
sraa1515 <sara.attallah-gherardi@hetic.net>
2018-01-12 15:11:50 +0100

    style App and portrait title

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,23 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,600,600i,700');  html,
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 300;
+    color: #FFF;
+    background-color: #000;
+  }
 
+  /* Portraits Style */
+
+  h1 {
+    font-size: 45px;
+    font-weight: bold;
+    line-height: 0.91;
+    text-align: left;
+    text-transform: uppercase;
+    padding: 5px 0;
+  }
 </style>
