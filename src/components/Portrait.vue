463e6df9bc888434464ab26fa98461f157d6f895
sraa1515 <sara.attallah-gherardi@hetic.net>
2018-02-10 16:27:29 +0100

    create PortraitToolbar Component and put it into Portrait component

--- src/components/Portrait.vue
+++ src/components/Portrait.vue
@@ -1,33 +1,40 @@
 <template>
   <div class="portrait">
-    <div class="portrait__slider-title">
-      <div class="portrait__slider-nickname-container">
-        <div class="dash"></div>
-        <span class="portrait__slider-nickname">&laquo Le guépard &raquo</span>
+    <portrait-toolbar></portrait-toolbar>
+    <div class="portrait-container">
+      <div class="portrait__slider-title">
+        <div class="portrait__slider-nickname-container">
+          <div class="dash"></div>
+          <span class="portrait__slider-nickname">&laquo Le guépard &raquo</span>
+        </div>
+        <h1>
+          Marie-Amélie Le Fur
+        </h1>
       </div>
-      <h1>
-        Marie-Amélie Le Fur
-      </h1>
-    </div>
-    <div class="portrait__slider-subtitle">
-      <p>Longueur, 100 m, 200 m, 400 m <p>
-      <p>Athlétisme</p>
+      <div class="portrait__slider-subtitle">
+        <p>Longueur, 100 m, 200 m, 400 m <p>
+        <p>Athlétisme</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import PortraitToolbar from '@/components/PortraitToolbar'
 export default {
-  name: 'Portrait',
-  data () {
-    return {}
+    name: 'Portrait',
+    components: {
+      'portrait-toolbar': PortraitToolbar
+    },
+    data () {
+      return {}
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .portrait {
+  .portrait-container {
     padding: 0 40px;
   }
   .portrait__slider-nickname-container {
---
+++ src/components/PortraitToolbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="portrait-toolbar">
+    <h2 class="portrait-toolbar__main-title">The real heroes</h2>
+    <img class="portrait-toolbar__menu" src="../assets/img/Menu.png" alt="menu">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PortraitToolbar',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .portrait-toolbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+  }
+  .portrait-toolbar__menu {
+    height: 20px;
+  }
+</style>
